72c013ef1372cce170ac4bf2d17a2599652c80ff
Vlad <demcenkovlad471@gmail.com>
2026-03-25 00:00:24 +0700

    ORM_model_route

--- src/main/java/com/walking/route_generator/model/Route.java
+++ src/main/java/com/walking/route_generator/model/Route.java
@@ -1,4 +1,28 @@
 package com.walking.route_generator.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "routes")
+
+@Getter @Setter
+
 public class Route {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String title; // Название маршрута
+    private Double totalDistance; //Дистанция маршрута
+    private String tempo; //Темп ходьбы
+
+    // Связь
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "rout_id")
+    private List<Point> points;
+
 }
