0776e77125acf4f0c1e2ddc07601ea4093c1e802
Vlad <demcenkovlad471@gmail.com>
2026-03-25 00:06:52 +0700

    ORM_model_user

--- src/main/java/com/walking/route_generator/model/User.java
+++ src/main/java/com/walking/route_generator/model/User.java
@@ -1,4 +1,33 @@
 package com.walking.route_generator.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@Getter @Setter
+
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password; // В будущем здесь будет хэш пароля
+
+    private String email;
+
+    // Связь:
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Route> savedRoutes = new ArrayList<>();
+
 }
