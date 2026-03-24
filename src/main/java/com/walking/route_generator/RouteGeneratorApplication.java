061807551b70973326c0b4bff65a586930bbe731
Vlad <demcenkovlad471@gmail.com>
2026-03-24 20:25:20 +0700

    innit commit

---
+++ src/main/java/com/walking/route_generator/RouteGeneratorApplication.java
@@ -0,0 +1,15 @@
+package com.walking.route_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouteGeneratorApplication.class, args);
+
+
+	}
+
+}
---
+++ src/test/java/com/walking/route_generator/RouteGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walking.route_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
