943e9d835649ea1c442d60c61c7d02dc770c80d2
Vlad <demcenkovlad471@gmail.com>
2026-05-03 18:52:51 +0700

    add validation for user

--- src/main/java/com/walking/route_generator/model/User.java
+++ src/main/java/com/walking/route_generator/model/User.java
@@ -1,6 +1,9 @@
 package com.walking.route_generator.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,13 +19,22 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
 
+    @NotBlank(message = "Имя пользователя обязательно")
+    @Size(min = 3, max = 50)
     @Column(unique = true, nullable = false)
     private String username;
 
+    @NotBlank(message = "Пароль обязателен")
+    @Size(min = 8, message = "Пароль должен быть не менее 8 символов")
     @Column(nullable = false)
     private String password; // В будущем здесь будет хэш пароля
 
+    @Email(message = "Некорректный формат email")
+    @NotBlank(message = "Email обязателен")
+    @Column(unique = true, nullable = false)
     private String email;
+
+    private String role = "ROLE_USER"; // По умолчанию обычный пользователь
 
     // Связь:
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
