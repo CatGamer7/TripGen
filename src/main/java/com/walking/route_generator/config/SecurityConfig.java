3756e9acb9e6abbe3c05b3ac442bcf3f1e10476b
Vlad <demcenkovlad471@gmail.com>
2026-05-19 11:16:18 +0700

    update connect with front

--- src/main/java/com/walking/route_generator/config/SecurityConfig.java
+++ src/main/java/com/walking/route_generator/config/SecurityConfig.java
@@ -24,10 +24,7 @@ public class SecurityConfig {
                     CorsConfiguration config = new CorsConfiguration();
                     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     config.setAllowedHeaders(List.of("*"));
-                    config.setAllowedOrigins(List.of(
-                            "http://localhost:5173",
-                            "*"
-                    ));
+                    config.setAllowedOriginPatterns(List.of("*"));
                     config.setAllowCredentials(true);
                     return config;
                 }))
